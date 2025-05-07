3e3e27ad31578753be92f06c9fa66c955a1e33ed
niko5606 <marknikolingad@gmail.com>
2025-05-07 19:58:09 +0800

    Add files via upload

---
+++ dhvsu parking/home page/homepage.js
@@ -0,0 +1,76 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js"
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyB_rXWCXqQdi6tshyUiKLiDfSKqMzqu6KQ",
+    authDomain: "login-b3b32.firebaseapp.com",
+    projectId: "login-b3b32",
+    storageBucket: "login-b3b32.firebasestorage.app",
+    messagingSenderId: "1078150727311",
+    appId: "1:1078150727311:web:42c7bde4a5482c5daad2fa"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+
+  const auth=getAuth();
+  const db=getFirestore();
+
+
+
+  const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+  allSideMenu.forEach(item=> {
+      const li = item.parentElement;
+  
+      item.addEventListener('click', function () {
+          allSideMenu.forEach(i=> {
+              i.parentElement.classList.remove('active');
+          })
+          li.classList.add('active');
+      })
+  });
+
+
+
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists()){
+                const userData=docSnap.data();
+                document.getElementById('loggedUserFName').innerText=userData.firstName;
+                document.getElementById('loggedUserEmail').innerText=userData.email;
+                document.getElementById('loggedUserLName').innerText=userData.lastName;
+
+            }
+            else{
+                console.log("no document found matching id")
+            }
+        })
+        .catch((error)=>{
+            console.log("Error getting document");
+        })
+    }
+    else{
+        console.log("User Id not Found in Local storage")
+    }
+  })
+
+  const logoutButton=document.getElementById('logout');
+
+  logoutButton.addEventListener('click',()=>{
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+    .then(()=>{
+        window.location.href='../login page/index.html';
+    })
+    .catch((error)=>{
+        console.error('Error Signing out:', error);
+    })
+  })
